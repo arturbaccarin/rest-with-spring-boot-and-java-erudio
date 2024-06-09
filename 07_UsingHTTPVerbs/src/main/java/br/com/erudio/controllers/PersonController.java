729919de13cd52b0b4266e1c9c219862570cce38
arturbaccarin <baccarin.artur@gmail.com>
2024-06-09 19:36:29 -0300

    82. 0903 Implementando o Padrão de Projetos VO

--- 07_UsingHTTPVerbs/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 07_UsingHTTPVerbs/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.erudio.model.Person;
+import br.com.erudio.data.vo.v1.PersonVO;
 import br.com.erudio.services.PersonServices;
 
 @RestController
@@ -25,25 +25,25 @@ public class PersonController {
     private PersonServices service;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> findAll() {
+    public List<PersonVO> findAll() {
         return service.findAll();
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(
+    public PersonVO findById(
             @PathVariable(value = "id") Long id) throws Exception {
         return service.findById(id);
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Person create(
-            @RequestBody Person person) {
+    public PersonVO create(
+            @RequestBody PersonVO person) {
         return service.create(person);
     }
 
     @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Person update(
-            @RequestBody Person person) {
+    public PersonVO update(
+            @RequestBody PersonVO person) {
         return service.update(person);
     }
 
--- 07_UsingHTTPVerbs/src/main/java/br/com/erudio/services/PersonServices.java
+++ 07_UsingHTTPVerbs/src/main/java/br/com/erudio/services/PersonServices.java
@@ -6,8 +6,8 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.erudio.data.vo.v1.PersonVO;
 import br.com.erudio.exceptions.ResourceNotFoundException;
-import br.com.erudio.model.Person;
 import br.com.erudio.repositories.PersonRepository;
 
 @Service
@@ -18,26 +18,26 @@ public class PersonServices {
     @Autowired
     PersonRepository repository;
 
-    public List<Person> findAll() {
+    public List<PersonVO> findAll() {
         logger.info("Finding people!");
 
         return repository.findAll();
     }
 
-    public Person findById(Long id) {
+    public PersonVO findById(Long id) {
         logger.info("Finding one person!");
 
         return repository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
     }
 
-    public Person create(Person person) {
+    public PersonVO create(PersonVO person) {
         logger.info("Creating one person!");
 
         return repository.save(person);
     }
 
-    public Person update(Person person) {
+    public PersonVO update(PersonVO person) {
         logger.info("Updating one person!");
 
         var entity = repository.findById(person.getId())
