8dbe54020a88fff554db0c10e67534623921444e
arturbaccarin <baccarin.artur@gmail.com>
2024-06-11 08:07:50 -0300

    86. 0906 Conclusão da Seção - O Padrão de Projetos VO (Value Object)

--- 07_UsingHTTPVerbs/src/main/java/br/com/erudio/services/PersonServices.java
+++ 07_UsingHTTPVerbs/src/main/java/br/com/erudio/services/PersonServices.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 
 import br.com.erudio.data.vo.v1.PersonVO;
 import br.com.erudio.exceptions.ResourceNotFoundException;
+import br.com.erudio.mapper.DozerMapper;
+import br.com.erudio.model.Person;
 import br.com.erudio.repositories.PersonRepository;
 
 @Service
@@ -21,20 +23,25 @@ public class PersonServices {
     public List<PersonVO> findAll() {
         logger.info("Finding people!");
 
-        return repository.findAll();
+        return DozerMapper.parseListObjects(repository.findAll(), PersonVO.class);
     }
 
     public PersonVO findById(Long id) {
         logger.info("Finding one person!");
 
-        return repository.findById(id)
+        var entity = repository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        return DozerMapper.parseObject(entity, PersonVO.class);
     }
 
     public PersonVO create(PersonVO person) {
         logger.info("Creating one person!");
 
-        return repository.save(person);
+        var entity = DozerMapper.parseObject(person, Person.class);
+        var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+
+        return vo;
     }
 
     public PersonVO update(PersonVO person) {
@@ -48,7 +55,9 @@ public class PersonServices {
         entity.setAddress(person.getAddress());
         entity.setGender(person.getGender());
 
-        return repository.save(entity);
+        var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+
+        return vo;
     }
 
     public void delete(Long id) {
