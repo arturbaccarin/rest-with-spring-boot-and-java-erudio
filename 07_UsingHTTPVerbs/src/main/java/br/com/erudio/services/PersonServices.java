c00f850808c308a84e5665765bafc70ef6afecb9
arturbaccarin <baccarin.artur@gmail.com>
2024-06-12 06:42:05 -0300

    1003 Criando um Mapper Customizado

--- 07_UsingHTTPVerbs/src/main/java/br/com/erudio/services/PersonServices.java
+++ 07_UsingHTTPVerbs/src/main/java/br/com/erudio/services/PersonServices.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.data.vo.v2.PersonVOV2;
 import br.com.erudio.exceptions.ResourceNotFoundException;
 import br.com.erudio.mapper.DozerMapper;
+import br.com.erudio.mapper.custom.PersonMapper;
 import br.com.erudio.model.Person;
 import br.com.erudio.repositories.PersonRepository;
 
@@ -19,6 +21,9 @@ public class PersonServices {
 
     @Autowired
     PersonRepository repository;
+
+    @Autowired
+    private PersonMapper mapper;
 
     public List<PersonVO> findAll() {
         logger.info("Finding people!");
@@ -42,6 +47,15 @@ public class PersonServices {
         var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
 
         return vo;
+    }
+
+    public PersonVOV2 createV2(PersonVOV2 person) {
+        logger.info("Creating one person!");
+
+        var entity = mapper.convertVOToEntity(person);
+        var vov2 = mapper.convertEntityToVO(repository.save(entity));
+
+        return vov2;
     }
 
     public PersonVO update(PersonVO person) {
